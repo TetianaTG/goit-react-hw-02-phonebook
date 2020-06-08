0bc14834254c35d3698abf00c48b193ab1c279ac
Татьяна <tatana@MacBook-Pro-Tatana.local>
2020-06-08 17:26:23 +0300

    Updates

---
+++ precache-manifest.e5a4082a87d35364a6e81e509ca18c03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adc3d4d834876761e391797db7d58977",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "6c6470b534ca934a3a5a",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.26c3ba85.chunk.css"
+  },
+  {
+    "revision": "437b5268025319aa06c0",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.9851ee21.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.9851ee21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c6470b534ca934a3a5a",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.42015566.chunk.js"
+  },
+  {
+    "revision": "c97d5bc26709c96968e6",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.76dda4f9.js"
+  }
+]);
